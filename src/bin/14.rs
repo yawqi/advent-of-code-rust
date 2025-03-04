6ad12a5fd8df80c9032d4e60b40add6565d7414a
Ted Wang <ted.wang@nio.com>
2025-03-04 11:06:45 +0800

    2020 day 14 redo

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,139 +1,133 @@
 use std::collections::HashMap;
 
+use itertools::Itertools;
+
 advent_of_code::solution!(14);
 
-#[derive(Debug, Clone, Copy)]
-struct Mask<'a>(&'a str);
+#[derive(Copy, Clone, Debug)]
+struct Mask {
+    to_set: u64,
+    to_clean: u64,
+    to_float: u64,
+}
 
-impl std::ops::BitAnd<u64> for Mask<'_> {
-    type Output = u64;
-    fn bitand(self, rhs: u64) -> Self::Output {
-        self.0
-            .bytes()
-            .rev()
-            .enumerate()
-            .fold(rhs, |rhs, (idx, byte)| match byte {
-                b'X' => rhs,
-                b'0' => rhs & !(1u64 << idx),
-                b'1' => rhs | (1u64 << idx),
-                _ => unreachable!(),
+impl Mask {
+    pub fn new(bits: &str) -> Self {
+        let mut to_set = 0u64;
+        let mut to_clean = 0u64;
+        let mut to_float = 0u64;
+        bits.bytes().rev().enumerate().for_each(|(idx, b)| match b {
+            b'0' => to_clean |= 1 << idx,
+            b'1' => to_set |= 1 << idx,
+            b'X' => to_float |= 1 << idx,
+            _ => unreachable!(),
+        });
+
+        Mask {
+            to_set,
+            to_clean,
+            to_float,
+        }
+    }
+
+    pub fn apply(&self, value: u64) -> u64 {
+        (value | self.to_set) & !self.to_clean
+    }
+
+    pub fn x_powers(&self) -> impl Iterator<Item = Mask> {
+        let mut new_mask = *self;
+        let to_float = self.to_float;
+        new_mask.to_clean = to_float;
+
+        (0..64u64)
+            .filter(move |&idx| (to_float & (1 << idx)) != 0)
+            .powerset()
+            .map(move |floats_to_set| {
+                let mut curr_mask = new_mask;
+                for idx in floats_to_set {
+                    curr_mask.to_set |= 1 << idx;
+                    curr_mask.to_clean &= !(1 << idx);
+                }
+                curr_mask
             })
     }
 }
 
-impl Mask<'_> {
-    pub fn memory_address_and(&self, address: u64) -> impl Iterator<Item = u64> {
-        let mut floating_bits = vec![];
-        let mut base_value = address;
-        self.0
-            .bytes()
-            .rev()
-            .enumerate()
-            .for_each(|(idx, c)| match c {
-                b'X' => {
-                    floating_bits.push(idx);
-                    base_value &= !(1 << idx);
+#[derive(Debug, Copy, Clone)]
+enum Instruction {
+    SetMaskInst(Mask),
+    WriteMemInst(u64, u64),
+}
+
+impl Instruction {
+    pub fn parse(line: &str) -> Self {
+        peg::parser! {
+            pub(crate) grammar parser() for str {
+                pub rule instruction() -> Instruction
+                = v:(set_mask() / write_mem()) whitespaces() { v }
+
+                rule set_mask() -> Instruction
+                = "mask = " bits:$(['X' | '0' | '1']+) {
+                    let mask = Mask::new(bits);
+                    Instruction::SetMaskInst(mask)
                 }
-                b'1' => base_value |= 1 << idx,
-                b'0' => {}
-                _ => unreachable!(),
-            });
 
-        let permutation_count = 2u64.pow(floating_bits.len() as u32);
-        (0..permutation_count).map(move |nth| {
-            (0..floating_bits.len())
-                .filter(|&bitn| (nth & (1 << bitn)) != 0)
-                .fold(base_value, |curr, index_to_bit_to_set| {
-                    let bitn = floating_bits[index_to_bit_to_set];
-                    curr | (1 << bitn)
-                })
-        })
-    }
-}
+                rule write_mem() -> Instruction
+                = "mem[" addr:number() "] = " value:number() {
+                    Instruction::WriteMemInst(addr, value)
+                }
 
-struct Emulator {
-    memory: HashMap<u64, u64>,
-}
+                rule number() -> u64
+                = e:$(['0'..='9']+) { e.parse().unwrap() }
 
-impl Emulator {
-    fn emulate(&mut self, mask: Mask, writes: Vec<(u64, u64)>) {
-        writes.into_iter().for_each(|(idx, value)| {
-            let new_value = mask & value;
-            self.memory.insert(idx, new_value);
-        });
-    }
-
-    fn emulate_2(&mut self, mask: Mask, writes: Vec<(u64, u64)>) {
-        writes
-            .into_iter()
-            .flat_map(|(addr, value)| mask.memory_address_and(addr).zip(std::iter::repeat(value)))
-            .for_each(|(addr, value)| {
-                self.memory.insert(addr, value);
-            });
-    }
-
-    fn final_sum(&self) -> u64 {
-        self.memory.values().sum()
-    }
-}
-
-use regex::Regex;
-pub fn part_one(input: &str) -> Option<u64> {
-    let mut lines = input.lines().peekable();
-    let mut emulator = Emulator {
-        memory: HashMap::new(),
-    };
-
-    let re = Regex::new(r"[\d]+").unwrap();
-    while let Some(mask) = lines.next() {
-        let mask = Mask(mask.strip_prefix("mask = ").unwrap());
-        let mut writes = vec![];
-
-        loop {
-            let peek = lines.peek();
-            if peek.is_none() || peek.unwrap().starts_with("mask") {
-                break;
+                rule whitespaces()
+                = [' ' | '\t' | '\n']*
             }
-            let write_op = lines.next().unwrap();
-            let mut v = re
-                .captures_iter(write_op)
-                .map(|v| v.extract::<0>().0.parse::<u64>().unwrap());
-            writes.push((v.next().unwrap(), v.next().unwrap()));
         }
 
-        emulator.emulate(mask, writes);
+        parser::instruction(line).unwrap()
     }
-
-    Some(emulator.final_sum())
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut lines = input.lines().peekable();
-    let mut emulator = Emulator {
-        memory: HashMap::new(),
-    };
-
-    let re = Regex::new(r"[\d]+").unwrap();
-    while let Some(mask) = lines.next() {
-        let mask = Mask(mask.strip_prefix("mask = ").unwrap());
-        let mut writes = vec![];
-
-        loop {
-            let peek = lines.peek();
-            if peek.is_none() || peek.unwrap().starts_with("mask") {
-                break;
+    let insts = input.lines().map(Instruction::parse).collect::<Vec<_>>();
+    let mut mask;
+    let mut mems = HashMap::new();
+    let mut addr_masks = vec![];
+    for inst in insts {
+        match inst {
+            Instruction::SetMaskInst(new_mask) => {
+                mask = new_mask;
+                addr_masks = mask.x_powers().collect();
             }
-            let write_op = lines.next().unwrap();
-            let mut v = re
-                .captures_iter(write_op)
-                .map(|v| v.extract::<0>().0.parse::<u64>().unwrap());
-            writes.push((v.next().unwrap(), v.next().unwrap()));
+            Instruction::WriteMemInst(addr, value) => {
+                addr_masks.iter().for_each(|amask| {
+                    mems.insert(amask.apply(addr), value);
+                });
+            }
         }
-
-        emulator.emulate_2(mask, writes);
     }
+    Some(mems.values().sum())
+}
 
-    Some(emulator.final_sum())
+pub fn part_one(input: &str) -> Option<u64> {
+    let insts = input.lines().map(Instruction::parse).collect::<Vec<_>>();
+    let mut mask = Mask {
+        to_set: 0,
+        to_clean: 0,
+        to_float: 0,
+    };
+    let mut mems = HashMap::new();
+
+    for inst in insts {
+        match inst {
+            Instruction::SetMaskInst(new_mask) => mask = new_mask,
+            Instruction::WriteMemInst(addr, value) => {
+                mems.insert(addr, mask.apply(value));
+            }
+        }
+    }
+    Some(mems.values().sum())
 }
 
 #[cfg(test)]
